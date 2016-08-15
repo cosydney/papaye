923c33fbaafd0f065eb976444a33ec7a9b4a29fc
Sydney Cohen <chnsydney@gmail.com>
2016-08-15 17:36:18 +0100

    started google login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,4 +271,10 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+    config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
+      scope: 'email, profile',
+      prompt: 'select_account',
+      image_aspect_ratio: "square",
+      image_size: 50
+
 end
