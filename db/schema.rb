18db385df17b00c0a19ba76cd1d5c5db415e6c41
Sydney Cohen <chnsydney@gmail.com>
2016-08-15 16:18:33 +0100

    Revert "create freelancers model"

--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815151305) do
+ActiveRecord::Schema.define(version: 20160815145804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "freelancers", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "company_name"
-    t.text     "address"
-    t.text     "company_nr"
-    t.integer  "vat"
-    t.string   "iban"
-    t.integer  "bank_nr"
-    t.integer  "branch_nr"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
