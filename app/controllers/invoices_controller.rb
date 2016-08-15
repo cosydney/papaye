9cd9ad67b66a89f0f15fcb0b6d193e91c169c479
Lina Garcia <garcia.j.lina@gmail.com>
2016-08-15 17:57:32 +0100

    added all TODOS to Invoice controller

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,5 +1,8 @@
 class InvoicesController < ApplicationController
 
   # TODO show the freelancer the dashboard (index)
-
+  # TODO create a new Invoice (new/create)
+  # TODO show a specific Invoice
+  # TODO edit an already existing Invoice (edit/update)
+  # TODO destroy a specific Invoice
 end
