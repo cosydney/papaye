d306ccae51d576a6502e518fd911a6925b7527d2
joao viana <j.bernardo.viana@hotmail.com>
2016-08-16 19:15:44 +0100

    Nested form 90%

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815154603) do
+ActiveRecord::Schema.define(version: 20160816175845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160815154603) do
     t.string   "address"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "email"
   end
 
   add_index "clients", ["user_id"], name: "index_clients_on_user_id", using: :btree
