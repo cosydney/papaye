4825cf8546e873f1da79b94a3492de28a2c0a285
Sydney Cohen <chnsydney@gmail.com>
2016-08-16 11:30:38 +0100

    create freelancer controller and registrations controller

---
+++ app/controllers/freelancers_controller.rb
@@ -0,0 +1,19 @@
+class FreelancersController < ApplicationController
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
