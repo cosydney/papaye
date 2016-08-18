c6be2cf06d5e3f2155fd97f45e33f311733340e4
Lina Garcia <garcia.j.lina@gmail.com>
2016-08-18 12:33:50 +0100

    delete app/views/user_mailer/send_invoice.txt (better practise to send html)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,11 +6,18 @@ class UserMailer < ApplicationMailer
   #   en.user_mailer.send_invoice_client.subject
   #
   def send_invoice_client(invoice_id)
+    # Instance variable => available in view
     @invoice = Invoice.find(invoice_id)
 
     # client email is saved in the object invoice
     @client_email = @invoice.client.email
 
-    mail to: "to@example.org"
+    # Set fl (freelancer) name
+    @fl_fist_name = @invoice.freelancer.first_name
+    @fl_last_name = @invoice.freelancer.last_name
+    @fl_name = "#{fl_fist_name} #{fl_last_name}"
+
+    #render a view in app/views/user_email
+    mail(to: @client_email, subject: "Invoice from #{fl_name}")
   end
 end
