5f05dc8619bf1ad51b437adfbf44a1be090bce22
Lina Garcia <garcia.j.lina@gmail.com>
2016-08-18 14:38:41 +0100

    mail working on rails c

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -13,11 +13,11 @@ class UserMailer < ApplicationMailer
     @client_email = @invoice.client.email
 
     # Set fl (freelancer) name
-    @fl_fist_name = @invoice.freelancer.first_name
+    @fl_first_name = @invoice.freelancer.first_name
     @fl_last_name = @invoice.freelancer.last_name
-    @fl_name = "#{fl_fist_name} #{fl_last_name}"
+    @fl_name = "#{@fl_first_name} #{@fl_last_name}"
 
     #render a view in app/views/user_email
-    mail(to: @client_email, subject: "Invoice from #{fl_name}")
+    mail(to: @client_email, subject: "Invoice from #{@fl_name}")
   end
 end
