a43c4443eba5f65593538adf1df803ec51565791
joao viana <j.bernardo.viana@hotmail.com>
2016-08-19 17:10:01 +0100

    Form almost done javascript needed

--- app/models/description.rb
+++ app/models/description.rb
@@ -1,3 +1,11 @@
 class Description < ActiveRecord::Base
   belongs_to :invoice
+
+  validates :description, presence: true
+
+  # THIS IS WRONG, JUST A TEST!!!!
+  validates_format_of :unit, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  validates_format_of :price, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  validates_format_of :amount, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  validates_format_of :vat_tax, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
