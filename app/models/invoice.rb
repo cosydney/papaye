d9734ee620e9a232be22046c31cd373882de0f67
Sydney Cohen <chnsydney@gmail.com>
2016-08-23 18:08:06 +0100

    trying to make the notifications better but bugged everything

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,14 +1,15 @@
 class Invoice < ActiveRecord::Base
   # Newsfeed gem
   include PublicActivity::Model
-  tracked owner: Proc.new{ |controller, model| controller.current_user }
   # ----
   belongs_to :freelancer
   belongs_to :client
   has_many :transitions, class_name: "InvoiceTransition", autosave: false
   has_many :descriptions, dependent: :destroy
-  before_destroy :save_activity
 
+  before_destroy :destroy_activity
+  after_create :create_activity
+  after_update :update_activity
 
   accepts_nested_attributes_for :client
   accepts_nested_attributes_for :descriptions, reject_if: :all_blank
@@ -39,19 +40,29 @@ class Invoice < ActiveRecord::Base
     # Calling method in user_mailer.rb, delivers later so user doesn't have to wait
     UserMailer.send_invoice_client(self.id, text).deliver_later
     self.email_sent_at = Time.now
-        create_activity( key: 'invoice.sent',
-                     owner: Proc.new{ |controller, model| controller.current_user },
-                     parameters:{ client_name: client.first_name }
-                    )
-
+    log_activity :sent
     save
   end
 
-  def save_activity
-    create_activity( key: 'invoice.destroy',
-                     owner: Proc.new{ |controller, model| controller.current_user },
-                     parameters:{ client_name: client.first_name }
-                    )
+  def destroy_activity
+    log_activity :destroy
   end
 
+  def update_activity
+    log_activity :update
+  end
+
+  def create_activity
+    log_activity :create
+  end
+
+  protected
+
+  def log_activity(type)
+    create_activity(
+      key: "invoice.#{type}",
+      owner: Proc.new{ |controller, model| controller.current_user },
+      parameters:{ client_name: client.first_name, client_last_name: client.last_name, company: client.company }
+    )
+  end
 end
