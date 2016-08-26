2209fa7a7cc140160973b8a3b46e8c68aa585717
Sydney Cohen <chnsydney@gmail.com>
2016-08-26 10:11:08 +0100

    beta and activity tracker for paid stuff

--- app/models/freelancer.rb
+++ app/models/freelancer.rb
@@ -13,7 +13,7 @@ class Freelancer < ActiveRecord::Base
 
   def set_email_text
     self.email_text =
-    "Like we agreed you will find here a copy of the invoice to be paid.  Click on the button below to be redirected for the payment. This will secure the money until the mission is finished, thanks to Invictus.
+    "Like we agreed you will find here a copy of the invoice to be paid.  Click on the button below to be redirected for the payment. This will secure the money until the mission is finished, thanks to Papaye.
 
 Look forward to start the mission
  Thank you,
